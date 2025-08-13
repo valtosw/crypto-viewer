8cf332d6dcf12bbc63700df16e8c3543ca66bd81
valtosw <volodijbessarabov@knu.ua>
2025-08-13 15:40:26 +0300

    add api client

---
+++ CryptoViewer.Infrastructure/Http/ApiClient.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CryptoViewer.Infrastructure.Http
+{
+    public sealed class ApiClient
+    {
+        //private const string _apiPath = "https://rest.coincap.io/v3/";
+
+        private readonly HttpClient _httpClient;
+        private readonly JsonSerializerOptions _jsonOptions;
+
+
+        public ApiClient(Uri baseAddress)
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = baseAddress,
+                Timeout = TimeSpan.FromSeconds(30)
+            };
+
+            _jsonOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                NumberHandling = JsonNumberHandling.AllowReadingFromString,
+            };
+        }
+
+        public static async Task<T> GetAsync<T>(string path, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
