32e6399c4a1e4254f583d5535addf5c4ea3b2a43
valtosw <volodijbessarabov@knu.ua>
2025-08-13 15:40:05 +0300

    add basic models

---
+++ CryptoViewer.Core/Models/Asset.cs
@@ -0,0 +1,16 @@
+﻿namespace CryptoViewer.Core.Models
+{
+    public class Asset
+    {
+        public required string Id { get; set; }
+        public required string Symbol { get; set; }
+        public required string Name { get; set; }
+        public int Rank { get; set; }
+        public decimal? Supply { get; set; }
+        public decimal? MaxSupply { get; set; }
+        public decimal? PriceUsd { get; set; }
+        public decimal? ChangePercent24Hr { get; set; }
+        public decimal? MarketCapUsd { get; set; }
+        public decimal? VolumeUsd24Hr { get; set; }
+    }
+}
---
+++ CryptoViewer.Core/Models/Market.cs
@@ -0,0 +1,17 @@
+﻿namespace CryptoViewer.Core.Models
+{
+    public class Market
+    {
+        public required string ExchangeId { get; set; }
+        public int Rank { get; set; }
+        public required string BaseSymbol { get; set; }
+        public required string BaseId { get; set; }
+        public required string QuoteSymbol { get; set; }
+        public required string QuoteId { get; set; }
+        public decimal? PriceQuote { get; set; }
+        public decimal? PriceUsd { get; set; }
+        public decimal? VolumeUsd24Hr { get; set; }
+        public decimal? PercentExchangeVolume { get; set; }
+        public decimal? TradesCount24Hr { get; set; }
+    }
+}
