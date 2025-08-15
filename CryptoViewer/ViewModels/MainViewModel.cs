210e1d75f3ef42a3c32019586c62aa782fd7a705
valtosw <volodijbessarabov@knu.ua>
2025-08-15 21:25:22 +0300

    add MainViewModel for asset management in wpf

---
+++ CryptoViewer/ViewModels/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using CryptoViewer.Core.Interfaces;
+using CryptoViewer.Core.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace CryptoViewer.UI.Wpf.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private readonly IMarketDataService _marketDataService;
+
+        public ObservableCollection<Asset> Assets { get; } = [];
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public MainViewModel(IMarketDataService marketDataService)
+        {
+            _marketDataService = marketDataService;
+            _ = LoadAssetsAsync();
+        }
+
+        protected void OnPropertyChanged(string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private async Task LoadAssetsAsync(CancellationToken cancellationToken = default)
+        {
+            var assets = await _marketDataService.GetTopAssetsAsync(100, cancellationToken);
+            Assets.Clear();
+
+            foreach (var asset in assets)
+            {
+                Assets.Add(asset);
+            }
+        }
+    }
+}
