5bcac86d9909cf65766328ff514e99435dca88df
valtosw <volodijbessarabov@knu.ua>
2025-08-15 21:25:38 +0300

    implement dependency injection and configuration setup

--- CryptoViewer/App.xaml.cs
+++ CryptoViewer/App.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using CryptoViewer.Core.Interfaces;
+using CryptoViewer.Infrastructure.Http;
+using CryptoViewer.Infrastructure.Services;
+using CryptoViewer.UI.Wpf.ViewModels;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
 
 namespace CryptoViewer
@@ -9,6 +13,39 @@ namespace CryptoViewer
     /// </summary>
     public partial class App : Application
     {
+        public static IServiceProvider ServiceProvider { get; private set; } = null!;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var apiBaseUrl = new Uri(config["CoinCap:BaseUrl"]!);
+            var apiKey = config["CoinCap:ApiKey"]!;
+
+            var services = new ServiceCollection();
+
+            services.AddSingleton(new ApiClient(apiBaseUrl, apiKey));
+
+            services.AddScoped<IMarketDataService, MarketDataService>();
+            services.AddScoped<ISearchService, SearchService>();
+            services.AddScoped<IConverterService, ConverterService>();
+
+            services.AddTransient<MainViewModel>();
+
+            ServiceProvider = services.BuildServiceProvider();
+
+            var mainWindow = new MainWindow
+            {
+                DataContext = ServiceProvider.GetRequiredService<MainViewModel>()
+            };
+
+            mainWindow.Show();
+        }
     }
 
 }
