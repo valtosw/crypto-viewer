d977e7b4597a7da465ff22f9969c07aa97302d3a
valtosw <volodijbessarabov@knu.ua>
2025-08-17 14:00:11 +0300

    add INavigationService interface and NavigationService class

---
+++ CryptoViewer/Services/Interfaces/INavigationService.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Controls;
+
+namespace CryptoViewer.UI.Wpf.Services.Interfaces
+{
+    public interface INavigationService
+    {
+        void SetFrame(Frame frame);
+        void NavigateTo<T>() where T : Page;
+    }
+}
---
+++ CryptoViewer/Services/NavigationService.cs
@@ -0,0 +1,19 @@
+﻿using CryptoViewer.UI.Wpf.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows.Controls;
+
+namespace CryptoViewer.UI.Wpf.Services
+{
+    public class NavigationService(IServiceProvider serviceProvider) : INavigationService
+    {
+        private Frame _frame = null!;
+
+        public void SetFrame(Frame frame) => _frame = frame;
+
+        public void NavigateTo<T>() where T : Page
+        {
+            var page = serviceProvider.GetRequiredService<T>();
+            _frame.Navigate(page);
+        }
+    }
+}
