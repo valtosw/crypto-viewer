0411094363bc12169bcd379bf5fdab8b5b55e9b3
valtosw <volodijbessarabov@knu.ua>
2025-08-17 22:42:04 +0300

    refactor for better modularity

--- CryptoViewer/ViewModels/AssetDetailViewModel.cs
+++ CryptoViewer/ViewModels/AssetDetailViewModel.cs
@@ -1,17 +1,35 @@
-﻿using CryptoViewer.Core.Models;
+﻿using CryptoViewer.Core.Interfaces;
+using CryptoViewer.Core.Models;
 using CryptoViewer.UI.Wpf.Services.Interfaces;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace CryptoViewer.UI.Wpf.ViewModels
 {
-    public class AssetDetailViewModel : INavigable
+    public class AssetDetailViewModel(IMarketDataService marketDataService) : INavigable
     {
         public Asset? Asset { get; set; }
 
-        public void OnNavigatedTo(object parameter)
+        public ObservableCollection<Market> Markets { get; } = [];
+
+        public async Task OnNavigatedTo(object parameter)
         {
             if (parameter is Asset asset)
             {
-                Asset = asset;
+                await LoadMarketsAsync(asset);
+            }
+        }
+
+        public async Task LoadMarketsAsync(Asset asset)
+        {
+            Asset = asset;
+            Markets.Clear();
+
+            var markets = await marketDataService.GetMarketsAsync(asset.Id);
+
+            foreach (var market in markets)
+            {
+                Markets.Add(market);
             }
         }
     }
