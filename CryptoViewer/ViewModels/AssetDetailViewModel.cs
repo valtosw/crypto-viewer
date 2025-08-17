3762e138b4fe5e914802a79a1df2843241fb610e
valtosw <volodijbessarabov@knu.ua>
2025-08-17 21:59:19 +0300

    enhance for navigation support

--- CryptoViewer/ViewModels/AssetDetailViewModel.cs
+++ CryptoViewer/ViewModels/AssetDetailViewModel.cs
@@ -1,6 +1,18 @@
-﻿namespace CryptoViewer.UI.Wpf.ViewModels
+﻿using CryptoViewer.Core.Models;
+using CryptoViewer.UI.Wpf.Services.Interfaces;
+
+namespace CryptoViewer.UI.Wpf.ViewModels
 {
-    public class AssetDetailViewModel
+    public class AssetDetailViewModel : INavigable
     {
+        public Asset? Asset { get; set; }
+
+        public void OnNavigatedTo(object parameter)
+        {
+            if (parameter is Asset asset)
+            {
+                Asset = asset;
+            }
+        }
     }
 }
