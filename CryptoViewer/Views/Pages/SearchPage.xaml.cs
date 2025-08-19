c0c320ae2dc793a1733a65aa0d13d46e0368fd44
valtosw <volodijbessarabov@knu.ua>
2025-08-19 18:54:53 +0300

    refactor SearchPage to use SearchViewModel

--- CryptoViewer/Views/Pages/SearchPage.xaml.cs
+++ CryptoViewer/Views/Pages/SearchPage.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using CryptoViewer.UI.Wpf.ViewModels;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CryptoViewer.UI.Wpf.Views.Pages
 {
@@ -20,9 +9,22 @@ namespace CryptoViewer.UI.Wpf.Views.Pages
     /// </summary>
     public partial class SearchPage : Page
     {
-        public SearchPage()
+        private readonly SearchViewModel _viewModel;
+
+        public SearchPage(SearchViewModel viewModel)
         {
             InitializeComponent();
+            DataContext = viewModel;
+            _viewModel = viewModel;
+        }
+
+        private async void SearchTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                e.Handled = true;
+                await _viewModel.PerformSearchAsync();
+            }
         }
     }
 }
